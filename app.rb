7d581d3ac5b9267746a7b2e31addf3f772ff06f4
Mike Chlipala <mike@chlipala.com>
2013-07-22 17:52:02 -0700

    whoopsies on the params

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ get '/' do
 	erb :home
 end
 
-get '/:from/:to' do
+get '/:name/:from' do
 	@message = "Hey #{params[:name]}, I think you're really nice."
 	@subtitle = "- #{params[:from]}"
 	erb :be_nice
