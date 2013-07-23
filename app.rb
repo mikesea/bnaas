f9d22fe5c45710a7627e50cde924d7fdc80cafc6
Mike Chlipala <mike@chlipala.com>
2013-07-22 19:09:51 -0700

    fix indenting issue

--- app.rb
+++ app.rb
@@ -2,25 +2,25 @@ require 'sinatra'
 require 'json'
 
 get '/' do
-	erb :home
+  erb :home
 end
 
 get '/:name/:from' do
-	message = "Hey #{params[:name]}, I think you're really nice."
-	subtitle = "- #{params[:from]}"
-	respond message, subtitle
+  message = "Hey #{params[:name]}, I think you're really nice."
+  subtitle = "- #{params[:from]}"
+  respond message, subtitle
 end
 
 def respond(message, subtitle)
-	case env['HTTP_ACCEPT']
-	when 'application/json'
-		content_type 'application/json'
-		{ message: message, subtitle: subtitle }.to_json
-	when 'text/plain'
-		content_type 'text/plain'
-		"#{message}\n#{subtitle}"
-	else
-		content_type 'text/html'
-		erb :be_nice, locals: { message: message, subtitle: subtitle }
-	end
+  case env['HTTP_ACCEPT']
+  when 'application/json'
+    content_type 'application/json'
+    { message: message, subtitle: subtitle }.to_json
+  when 'text/plain'
+    content_type 'text/plain'
+    "#{message}\n#{subtitle}"
+  else
+    content_type 'text/html'
+    erb :be_nice, locals: { message: message, subtitle: subtitle }
+  end
 end
