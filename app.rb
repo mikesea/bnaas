8f28eedb7d033554f76c8ee5652e558d25dcb3b3
Mike Chlipala <mike@chlipala.com>
2013-07-22 18:29:09 -0700

    add content negotiation

--- app.rb
+++ app.rb
@@ -1,11 +1,26 @@
 require 'sinatra'
+require 'json'
 
 get '/' do
 	erb :home
 end
 
 get '/:name/:from' do
-	@message = "Hey #{params[:name]}, I think you're really nice."
-	@subtitle = "- #{params[:from]}"
-	erb :be_nice
+	message = "Hey #{params[:name]}, I think you're really nice."
+	subtitle = "- #{params[:from]}"
+	respond message, subtitle
+end
+
+def respond(message, subtitle)
+	case env['HTTP_ACCEPT']
+	when 'application/json'
+		content_type 'application/json'
+		{ message: message, subtitle: subtitle }.to_json
+	when 'text/plain'
+		content_type 'text/plain'
+		"#{message}\n#{subtitle}"
+	else
+		content_type 'text/html'
+		erb :be_nice, locals: { message: message, subtitle: subtitle }
+	end
 end
